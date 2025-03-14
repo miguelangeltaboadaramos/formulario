26e970c02188aab17420fff17c15578f26f96f17
miguelangeltaboadaramos <miguelangeltaboada140@gmail.com>
2025-03-13 21:21:56 -0500

    agregado 2

--- src/main/java/formulario/demo/model/promedioModel.java
+++ src/main/java/formulario/demo/model/promedioModel.java
@@ -1,7 +1,40 @@
 package formulario.demo.model;
 
 public class promedioModel {
+    private Double nota1;
+    private Double nota2;
+    private Double nota3;
+    private Double notaFinal;
 
+    public Double getNota1() {
+        return nota1;
+    }
 
+    public void setNota1(Double nota1) {
+        this.nota1 = nota1;
+    }
 
+    public Double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(Double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public Double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(Double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public Double getNotaFinal() {
+        return notaFinal;
+    }
+
+    public void setNotaFinal(Double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
 }
